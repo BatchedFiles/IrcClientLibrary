ad548aff47da766cfb50a9049b862f1474417817
Эрик Замабувараев‐Ёмолкуу <erik@freebasic.su>
2020-03-28 06:28:57 +0700

    Файл компиляции

--- make.cmd
+++ make.cmd
@@ -1,44 +1,52 @@
 set CompilerDirectory=%ProgramFiles%\FreeBASIC
 
-set MainFile=Modules\DLLMain.bas
-set Classes=Classes\ClassFactory.bas Classes\IrcClient.bas
-set Modules=Modules\Guids.bas Modules\InitializeVirtualTables.bas Modules\Registry.bas
-set Resources=Resources.rc
-set OutputFile=BatchedFilesIrcClient.dll
+set MainFile=
+set Classes=
+set Forms=
+set Modules=IrcClient.bas SendData.bas ReceiveData.bas ParseData.bas GetIrcData.bas SendMessages.bas Network.bas AppendingBuffer.bas
+set References=
+set Resources=
 
-set IncludeFilesPath=-i Classes -i Interfaces -i Modules
-set IncludeLibraries=-l kernel32 -l Guids
-set Subsystem=console
-set ExeTypeKind=dll
+set OutputFile=libBatchedFilesIrcClient.a
+
+set IncludeFilesPath=-i Classes -i Forms -i Headers -i Interfaces -i Modules
+set IncludeLibraries=
+set ExeTypeKind=lib
 
 set MaxErrorsCount=-maxerr 1
 set MinWarningLevel=-w all
-set OptimizationLevel=-O 3
-set VectorizationLevel=-vec 0
 REM set UseThreadSafeRuntime=-mt
 
 REM set EnableShowIncludes=-showincludes
-REM set EnableVerbose=-v
+set EnableVerbose=-v
 REM set EnableRuntimeErrorChecking=-e
 REM set EnableFunctionProfiling=-profile
 
-set PROGRAM_VERSION_MAJOR=2
-set PROGRAM_VERSION_MINOR=0
-set PROGRAM_VERSION_BUILD=0
-set PROGRAM_VERSION_REVISION=%RANDOM%
+if "%1"=="service" (
+	set SERVICE_DEFINED=-d WINDOWS_SERVICE
+) else (
+	set SERVICE_DEFINED=
+	set PERFORMANCE_TESTING_DEFINED=-d PERFORMANCE_TESTING
+)
 
 if "%2"=="debug" (
 	set EnableDebug=debug
+	set OptimizationLevel=-O 0
+	set VectorizationLevel=-vec 0
 ) else (
 	set EnableDebug=release
+	set OptimizationLevel=-O 3
+	set VectorizationLevel=-vec 0
 )
 
 if "%3"=="withoutruntime" (
 	set WithoutRuntime=withoutruntime
+	set GUIDS_WITHOUT_MINGW=-d GUIDS_WITHOUT_MINGW=1
 ) else (
 	set WithoutRuntime=runtime
+	set GUIDS_WITHOUT_MINGW=
 )
 
-set CompilerParameters=-d PROGRAM_VERSION_MAJOR=%PROGRAM_VERSION_MAJOR% -d PROGRAM_VERSION_MINOR=%PROGRAM_VERSION_MINOR% -d PROGRAM_VERSION_BUILD=%PROGRAM_VERSION_BUILD% -d PROGRAM_VERSION_REVISION=%PROGRAM_VERSION_REVISION% %MaxErrorsCount% %UseThreadSafeRuntime% %IncludeLibraries% %IncludeFilesPath% %OptimizationLevel% %VectorizationLevel% %MinWarningLevel% %EnableFunctionProfiling% %EnableShowIncludes% %EnableVerbose% %EnableRuntimeErrorChecking%
+set CompilerParameters=%SERVICE_DEFINED% %PERFORMANCE_TESTING_DEFINED% %GUIDS_WITHOUT_MINGW% %MaxErrorsCount% %UseThreadSafeRuntime% %MinWarningLevel% %EnableFunctionProfiling% %EnableShowIncludes% %EnableVerbose% %EnableRuntimeErrorChecking% %IncludeFilesPath% %IncludeLibraries% %OptimizationLevel% %VectorizationLevel% 
 
-call translator.cmd "%MainFile% %Classes% %Modules% %Resources%" "%ExeTypeKind%" "%OutputFile%" "%CompilerDirectory%" "%CompilerParameters%" %EnableDebug% noprofile %WithoutRuntime%
+call translator.cmd "%MainFile% %Classes% %Forms% %Modules% %Resources%" "%ExeTypeKind%" "%OutputFile%" "%CompilerDirectory%" "%CompilerParameters%" %EnableDebug% noprofile %WithoutRuntime%
